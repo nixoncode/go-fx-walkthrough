be41ad711bce10739d79d8eb3e8ef49a15e13f99
Nixon Kosgei <nkosgey6@gmail.com>
2023-05-18 07:16:24 +0300

    added http server

--- main.go
+++ main.go
@@ -5,8 +5,39 @@
 // Time: 06:55
 package main
 
-import "go.uber.org/fx"
+import (
+	"context"
+	"fmt"
+	"go.uber.org/fx"
+	"net"
+	"net/http"
+)
 
 func main() {
-	fx.New().Run()
+	fx.New(
+		fx.Provide(NewHttpServer),
+		fx.Invoke(func(*http.Server) {}), // need to find out why we need this and if it's possible to avoid
+	).Run()
+}
+
+func NewHttpServer(lc fx.Lifecycle) *http.Server {
+	srv := &http.Server{Addr: ":8080"}
+
+	lc.Append(fx.Hook{
+		OnStart: func(ctx context.Context) error {
+			listener, err := net.Listen("tcp", srv.Addr)
+			if err != nil {
+				return err
+			}
+			fmt.Println("Http Server starting at", srv.Addr)
+			go func() {
+				_ = srv.Serve(listener)
+			}()
+			return nil
+		},
+		OnStop: func(ctx context.Context) error {
+			return srv.Shutdown(ctx)
+		},
+	})
+	return srv
 }
